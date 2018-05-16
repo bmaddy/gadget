5a9e2e5ace6854ced45f3fd6de0ccb582df86be8
Brian Maddy <brian@brianmaddy.com>
2018-05-16 13:19:40 -0500

    added project url

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject gadget "0.1.0"
   :description "A modified version of inspect-tree to work with Datomic entities."
-  :url "http://example.com/FIXME"
+  :url "https://github.com/bmaddy/gadget"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
