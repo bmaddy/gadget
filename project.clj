5811db74e30a51f45c35d0059af05d3ca3e6468e
Brian Maddy <brian@brianmaddy.com>
2018-05-15 21:54:31 -0500

    release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject gadget "0.1.0-SNAPSHOT"
+(defproject gadget "0.1.0"
   :description "A modified version of inspect-tree to work with Datomic entities."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
